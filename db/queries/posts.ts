e208a561ce74a9af718d8a2bdce1d51f204cc58d
david <andrianarivo.david@gmail.com>
2023-12-17 21:24:08 +0300

    feat: top post list

--- db/queries/posts.ts
+++ db/queries/posts.ts
@@ -1,13 +1,13 @@
 import {Post} from '@prisma/client'
 import {db} from '@/db'
 
-/*export type PostWithData = (Post & {
+export type PostWithData = (Post & {
   topic: { slug: string }
   user: { name: string | null }
   _count: { comments: number }
-})*/
+})
 
-export type PostWithData = Awaited<ReturnType<typeof fetchPostsByTopicSlug>>[number]
+//export type PostWithData = Awaited<ReturnType<typeof fetchPostsByTopicSlug>>[number]
 
 export function fetchPostsByTopicSlug(slug: string) {
   return db.post.findMany({
@@ -17,5 +17,19 @@ export function fetchPostsByTopicSlug(slug: string) {
       user: {select: {name: true}},
       _count: {select: {comments: true}}
     }
+  })
+}
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: [
+      { comments: { _count: 'desc' } },
+    ],
+    include: {
+      topic: {select: {slug: true}},
+      user: {select: {name: true, image: true}},
+      _count: {select: {comments: true}}
+    },
+    take: 5,
   })
 }
